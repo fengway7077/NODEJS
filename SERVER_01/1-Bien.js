f4d6a3125144d4272552a13a54437833886bca14
fengway7077 <fengway7077@yahoo.com>
2017-05-11 15:40:32 +0700

    Updating

---
+++ SERVER_01/1-Bien.js
@@ -0,0 +1,17 @@
+var n =  10;
+var  a = "ABC";
+console.log(n + a);
+
+var x1 = 4 ;
+var y1 = 5 ;
+console.log(x1 + y1);
+
+var z1 = "15" ;
+var s1 = "7" ;
+console.log(z1 + s1);
+
+var k1 = "15" ;
+k =parseInt(k1);
+var g1 = "7" ;
+g =parseInt(g1);
+console.log(k + g);
